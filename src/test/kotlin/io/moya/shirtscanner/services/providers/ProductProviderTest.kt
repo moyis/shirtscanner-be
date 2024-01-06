a1e5e7d3490b4ee294671c441d72086964ccf23c
MoyaF <faustomoya99@gmail.com>
2024-01-06 22:07:09 +0100

    fix: tests

--- src/test/kotlin/io/moya/shirtscanner/services/providers/ProductProviderTest.kt
+++ src/test/kotlin/io/moya/shirtscanner/services/providers/ProductProviderTest.kt
@@ -2,7 +2,6 @@ package io.moya.shirtscanner.services.providers
 
 import io.moya.shirtscanner.configuration.ProviderMetadata
 import io.moya.shirtscanner.models.SearchResult
-import io.moya.shirtscanner.services.cache.CacheService
 import io.moya.shirtscanner.services.fetchers.ProductsFetcher
 import org.assertj.core.api.AssertionsForClassTypes
 import org.junit.jupiter.api.BeforeEach
@@ -16,13 +15,11 @@ class ProductProviderTest {
 
     private val providerMetadata: ProviderMetadata = ProviderMetadata(providerName)
 
-    private val cacheService = MockCacheService()
-
     private lateinit var subject: ProductProvider
 
     @BeforeEach
     fun setUp() {
-        subject = ProductProvider(productsFetcher, providerMetadata, cacheService)
+        subject = ProductProvider(productsFetcher, providerMetadata)
     }
 
     @Test
@@ -31,10 +28,6 @@ class ProductProviderTest {
         val result = subject.search(q)
         AssertionsForClassTypes.assertThat(result.providerName).isEqualTo(providerName)
     }
-}
-
-private class MockCacheService : CacheService {
-    override fun <T> computeIfAbsent(key: String, remappingFunction: (String) -> T) = remappingFunction.invoke(key)
 }
 
 private class EmptyProductFetcher(private val url: String) : ProductsFetcher {
