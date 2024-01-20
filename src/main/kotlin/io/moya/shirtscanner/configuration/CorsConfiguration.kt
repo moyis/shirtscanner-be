a0e9f0607141a72eb4f5d587bd2b8a86006543a9
MoyaF <faustomoya99@gmail.com>
2024-01-20 23:47:42 +0100

    fix: format

--- src/main/kotlin/io/moya/shirtscanner/configuration/CorsConfiguration.kt
+++ src/main/kotlin/io/moya/shirtscanner/configuration/CorsConfiguration.kt
@@ -5,12 +5,10 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-
 @Configuration
 class CorsConfiguration(
     private val corsConfigurationProperties: CorsConfigurationProperties,
 ) : WebMvcConfigurer {
-
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
             .allowedOrigins(*corsConfigurationProperties.allowedOrigins.toTypedArray())
