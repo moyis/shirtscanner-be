436e49f44694ec2eee66adf6bb2e9d43e31fa03e
Fausto Moya <faustomoya99@gmail.com>
2024-12-08 13:52:20 +0100

    fix: specify base packages for ConfigurationPropertiesScan

--- src/main/kotlin/dev/moyis/shirtscanner/Shirtscanner.kt
+++ src/main/kotlin/dev/moyis/shirtscanner/Shirtscanner.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@ConfigurationPropertiesScan
+@ConfigurationPropertiesScan(basePackages = ["dev.moyis.shirtscanner.infrastructure.configuration.properties"])
 class Shirtscanner
 
 fun main(args: Array<String>) {
