5e59ed3410fbbf5d9e9e56459a203314d8417353
Fausto Moya <faustomoya99@gmail.com>
2025-02-03 11:50:08 +0100

    fix: register SSSW for reflection

--- src/main/kotlin/dev/moyis/shirtscanner/Shirtscanner.kt
+++ src/main/kotlin/dev/moyis/shirtscanner/Shirtscanner.kt
@@ -1,11 +1,13 @@
 package dev.moyis.shirtscanner
 
+import org.springframework.aot.hint.annotation.RegisterReflection
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 @ConfigurationPropertiesScan(basePackages = ["dev.moyis.shirtscanner.infrastructure.configuration.properties"])
+@RegisterReflection(classNames = ["com.github.benmanes.caffeine.cache.SSSW"])
 class Shirtscanner
 
 fun main(args: Array<String>) {
