877601f2ba0b031fd7be19e46192df46768f512e
Fausto Moya <37676075+moyis@users.noreply.github.com>
2026-09-18 18:39:48 -0300

    feat: stream yupoo image proxying instead of buffering (#177)
    Co-authored-by: moyis <fausto@moyis.dev>

--- src/main/kotlin/dev/moyis/shirtscanner/domain/api/ImageService.kt
+++ src/main/kotlin/dev/moyis/shirtscanner/domain/api/ImageService.kt
@@ -1,11 +1,13 @@
 package dev.moyis.shirtscanner.domain.api
 
 import dev.moyis.shirtscanner.domain.spi.ImageProvider
+import org.springframework.core.io.buffer.DataBuffer
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
 
 @Service
 class ImageService(
     private val imageProvider: ImageProvider,
 ) {
-    suspend fun get(path: String): ByteArray? = imageProvider.get(path)
+    fun get(path: String): Flux<DataBuffer> = imageProvider.get(path)
 }
